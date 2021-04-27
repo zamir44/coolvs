c09a4fad1c4cc3ae62c919edb4b82d94220f08d7
zamir44 <tooradicalxxi@gmail.com>
2021-04-27 14:12:58 +0200

    main section close to finish

--- components/Main.js
+++ components/Main.js
@@ -81,7 +81,9 @@ function Main() {
                         quality="100"
                     />
                 <div className="absolute inset-y-11 inset-x-20  flex justify-around z-20">
-                    <button type="button" className="bg-white w-full text-black opacity-80 uppercase font-bold tracking-wide">rezervo</button>
+                    <button type="button" className="bg-transparent w-full text-white opacity-80 uppercase font-bold tracking-wide">
+                        rezervo
+                    </button>
                 </div>
                     </a>
                 </Link>
@@ -92,9 +94,11 @@ function Main() {
             
             <Container className="sm:hidden md:max-w-screen-md mx-auto  md:flex md:flex-row">
                 <Row>
-                    <Col  className="relative">
+                    <Col  className="relative p-0">
                         <div className="w-full absolute inset-0 bg-gradient-to-t from-black to-black opacity-30 z-10"></div>
+                        
                         <div  style={{ height: '500px', width:'500px'}} className="z-50">
+                        <div className="flex flex-col items-center">
                             <Image
                                 src="/../public/images/sunglasses.jpeg"
                                 alt="Picture of the author"
@@ -102,99 +106,72 @@ function Main() {
                                 quality="100"
                                 objectFit="cover"
                             />
-                        </div>
-                            
-                        <button type="button" className="uppercase bg-white text-black font-bold border border-white p-10 inset-0 absolute">
-                            <ul>
-                                <li>D</li>
-                                <li>I</li>
-                                <li>E</li>
-                                <li>L</li>
-                                <li>L</li>
-                                <li>I</li>
-                            </ul>
+                        <button type="button" className="uppercase bg-transparent text-white font-bold border border-white py-6 px-32 absolute z-50 bottom-7 tracking-wider">
+                            dielli
                         </button>
+                    </div>
+                        </div>
+                        
                     </Col>
 
-                    <Col size="6" className="relative ml-10">
+                    <Col size="6" className="relative ml-10 p-0">
                         <div className="w-full absolute inset-0 bg-gradient-to-t from-black to-black opacity-30 z-10"></div>
                         <div  style={{ height: '500px', width:"500px"}} className="z-50" >
-                            <Image
-                                src="/../public/images/optic.jpg"
-                                alt="Picture of the author"
-                                layout="fill"
-                                quality="100"
-                                objectFit="cover"
-                            />
+                            <div className="flex flex-col items-center">    
+                                <Image
+                                    src="/../public/images/optic.jpg"
+                                    alt="Picture of the author"
+                                    layout="fill"
+                                    quality="100"
+                                    objectFit="cover"
+                                />
+                                <button type="button" className="uppercase bg-transparent text-white font-bold border border-white py-6 px-32 absolute z-50 bottom-7 tracking-wider">
+                                    optike
+                                </button>
+                            </div>
                         </div>
-                            
-                        <button type="button" className="uppercase bg-white text-black font-bold border border-white p-10 inset-0 absolute">
-                            <ul>
-                                <li>O</li>
-                                <li>P</li>
-                                <li>T</li>
-                                <li>I</li>
-                                <li>K</li>
-                                <li>E</li>
-                            </ul>
-                        </button>
                     </Col>
                 </Row>
             </Container>
 
             <Container className="sm:hidden md:max-w-screen-md mx-auto mt-9 md:flex md:flex-row">
-                    <Row>
-                    <Col  className="relative">
-                    <div className="w-full absolute inset-0 bg-gradient-to-t from-black to-black opacity-30 z-10"></div>
-                    <div  style={{ height: '500px', width:'500px'}} className="z-50">
-                        <Image
-                            src="/../public/images/accessor.jpg"
-                            alt="Picture of the author"
-                            layout="fill"
-                            quality="100"
-                        />
-                    </div>
-                        
-                    <button type="button" className="uppercase bg-white text-black font-bold border border-white p-10 inset-0 absolute">
-                        <ul>
-                            <li>A</li>
-                            <li>K</li>
-                            <li>S</li>
-                            <li>E</li>
-                            <li>S</li>
-                            <li>O</li>
-                            <li>R</li>
-                            <li>Ë</li>
-                        </ul>
-                    </button>
-                </Col>
+                <Row>
+                    <Col  className="relative p-0">
+                        <div className="w-full absolute inset-0 bg-gradient-to-t from-black to-black opacity-30 z-10"></div>
+                        <div  style={{ height: '500px', width:'500px'}} className="z-50">
+                            <div className="flex flex-col items-center">
+                                <Image
+                                    src="/../public/images/accessor.jpg"
+                                    alt="Picture of the author"
+                                    layout="fill"
+                                    quality="100"
+                                />
+                                <button type="button" className="uppercase bg-transparent text-white font-bold border border-white py-6 px-32 absolute z-50 bottom-7 tracking-wider">
+                                    aksesorë
+                                </button>
+                            </div>
+                        </div>
+                    </Col>
 
-                <Col size="6" className="relative ml-10">
-                    <div className="w-full absolute inset-0 bg-gradient-to-t from-black to-black opacity-30 z-10"></div>
-                    <div  style={{ height: '500px', width:"500px"}} className="z-50" >
+                <Col size="6" className="relative ml-10 p-0">
+                    <div className="w-full absolute inset-0 bg-gradient-to-t from-black to-black opacity-30 z-10 "></div>
+                    <div  style={{ height: '500px', width:"500px", position: "relative"}} className="z-40" >
+                    <div className="flex flex-col items-center">
                         <Image
                             src="/../public/images/calendar.jpg"
                             alt="Picture of the author"
                             layout="fill"
                             quality="100"
+                            
                         />
+                        <button type="button" className="uppercase bg-transparent text-white font-bold border border-white py-6 px-32 absolute z-50 bottom-7 tracking-wider">
+                            rezervo 
+                        </button>
                     </div>
-                        
-                    <button type="button" className="uppercase bg-white text-black font-bold border border-white p-10 inset-0 absolute">
-                        <ul>
-                            <li>R</li>
-                            <li>E</li>
-                            <li>Z</li>
-                            <li>E</li>
-                            <li>R</li>
-                            <li>V</li>
-                            <li>O</li>
-                        </ul>
-                    </button>
-                    
+                    </div>
                 </Col>
                 
-                    </Row>
+                </Row>
             </Container>
         </>
     )
