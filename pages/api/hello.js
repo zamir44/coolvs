8a7cdf01f97cbf9c3b350511488e1f931f9fa8f3
zamir44 <tooradicalxxi@gmail.com>
2021-06-14 14:32:17 +0200

    api props removed

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req:, res:) => {
+export default (req, res) => {
   res.status(200).json({ name: 'John Doe' })
 }
